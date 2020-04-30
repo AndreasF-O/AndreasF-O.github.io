f9a23bbcdab88835af28fccf4b7e1bae0d03c9d8
AndreasF-O <andreasfo@gmail.com>
2020-04-30 19:59:11 +0200

    Added hashing router in main app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
+import { HashRouter } from "react-router-dom";
 import './App.css';
 import Pages from "./pages";
 import Layout from "./components/Layout";
 function App() {
   return (
-    <div>
-      <Layout />
-      <Pages />
-    </div>
+    <HashRouter basename="/">
+      <div>
+        <Layout />
+        <Pages />
+      </div>
+    </HashRouter>
   )
 }
 
